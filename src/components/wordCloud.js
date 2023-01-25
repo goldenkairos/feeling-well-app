70b8427d2dc3641cdcaf9c64685e6e080f12ed2d
Minh <minh.seikel@gmail.com>
2023-01-25 14:08:14 -0800

    clean up unused codes

--- src/components/wordCloud.js
+++ src/components/wordCloud.js
@@ -1,22 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+
 import ReactWordcloud from "react-wordcloud";
-
-// import "tippy.js/dist/tippy.css";
-// import "tippy.js/animations/scale.css";
-
-// import words from "./words";
-// const words = [
-//   { Text: "happy", Value: 1 },
-//   { Text: "wonderful", Value: 1 },
-//   { Text: "amused", Value: 1 },
-//   { Text: "confident", Value: 1 },
-//   { Text: "empowered", Value: 1 },
-//   { Text: "creative", Value: 100 },
-//   { Text: "cheerful", Value: 4 },
-//   { Text: "proud", Value: 1 },
-//   { Text: "grateful", Value: 1000 },
-// ]
 
 function WordCloud({ wordsFreq }) {
   const word2 = [];
@@ -27,10 +11,6 @@ function WordCloud({ wordsFreq }) {
     });
   }
 
-  // const word2 = words.map((d) => ({
-  //   text: d.Text,
-  //   value: d.Value
-  // })
 
   return (
     <div style={{ height: 200, width: 600 }}>
@@ -47,8 +27,5 @@ function WordCloud({ wordsFreq }) {
     </div>
   );
 }
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(<WordCloud />, rootElement);
 
 export default WordCloud;
